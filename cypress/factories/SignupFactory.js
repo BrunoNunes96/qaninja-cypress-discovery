b337427d06821af702bcc9781500ec2b258a8ed2
Brunonunesobj <bruno.nunes@objective.com.br>
2022-01-17 18:32:54 -0300

    First commit git

---
+++ cypress/factories/SignupFactory.js
@@ -0,0 +1,30 @@
+
+export default{
+
+    deliver: function() {
+
+
+    
+var data = {
+    name: 'Bruno Nunes',
+    cpf: '00000014141',
+    email: 'bruno@gmail.com',
+    whatsapp: '41999999999',
+    address: {
+        postalcode: '04534011',
+        street: 'Rua Joaquim Floriano',
+        number: '1000',
+        details: 'Ap 142',
+        district: 'Itaim Bibi',
+        city_state: 'São Paulo/SP'
+    },
+
+    delivery_method: 'Moto',
+    cnh: 'cnh-digital.jpg'
+
+    }
+
+    return data
+
+  }
+}
