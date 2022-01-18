edfd92e0064a6c49c38b504863591067d57d0e9e
Brunonunesobj <bruno.nunes@objective.com.br>
2022-01-18 09:14:36 -0300

    Config url

--- cypress/pages/singupPage.js
+++ cypress/pages/singupPage.js
@@ -2,7 +2,7 @@
 class SingupPage {
 
     go() {
-        cy.visit('/')
+        cy.visit('https://buger-eats-qa.vercel.app')
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
  
